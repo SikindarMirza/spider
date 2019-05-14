97613c9c0fd561fbd9e965c43dc28c4dc72aef53
INE12362345 <sikindar.mirza@tesco.com>
2019-05-14 23:11:43 +0530

    first commit

---
+++ src/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+    background: #777;
+    height: 3rem;
+    width: 100%;
+    margin: 0px;
+`
+export const LoginContainer = styled.div`
+    margin: 10rem 20rem;
+    background: #fff;
+    border: 2px solid #000;
+`
